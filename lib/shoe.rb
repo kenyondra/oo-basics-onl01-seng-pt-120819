f9e7ee163b2fee6d56f66af59e65724fd5945721
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-01-18 06:45:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,11 @@
 class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :brand
+  
   def initialize(brand)
     @brand = brand
   end
   
-  def brand=(brand)
-    @brand = brand
-  end
-  
-  def brand
-    @brand
-  end
-  
+  def cobble 
+    
 end
